33411531bf2e1a8465e52fba34870eb1c7f227c1
Sk7Str1pe <finenkofedor@gmail.com>
2025-01-08 20:48:10 +0300

    trying to get extension managed declaratively

--- web/firefox.nix
+++ web/firefox.nix
@@ -1,9 +1,19 @@
-{
+let
+  mkExtension = id: install_url: {
+    ${id} = {
+      inherit install_url;
+      installation_mode = "force_installed";
+    };
+  };
+in {
   programs.firefox = {
     enable = true;
     languagePacks = [
       "en-GB"
       "ru"
+    ];
+    profiles.Sk7Str1p3.extensions = [
+      (mkExtension "cliget@zaidabdulla.com" "https://addons.mozilla.org/firefox/downloads/latest/cliget/latest.xpi")
     ];
     #policies = ;
   };
