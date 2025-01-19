0ec3c87fb762653c2ae882d096916d8a4c45500d
Sk7Str1pe <finenkofedor@gmail.com>
2025-01-19 11:38:35 +0300

    firefox.nix => start fireFox config

--- web/firefox.nix
+++ web/firefox.nix
@@ -1,10 +1,48 @@
-{
+{pkgs, ...}: {
   programs.firefox = {
     enable = true;
+
     languagePacks = [
       "en-GB"
       "ru"
     ];
-    #policies = ;
+    policies = {
+      DefaultDownloadDirectory = "/media/HDD/Downloads";
+    };
+
+    profiles.Sk7Str1p3 = {
+      bookmarks = [
+        {
+          name = "wikipedia";
+          tags = ["wiki"];
+          keyword = "wiki";
+          url = "https://en.wikipedia.org/wiki/Special:Search?search=%s&go=Go";
+        }
+        {
+          name = "kernel.org";
+          url = "https://www.kernel.org";
+        }
+        {
+          name = "Nix sites";
+          toolbar = true;
+          bookmarks = [
+            {
+              name = "homepage";
+              url = "https://nixos.org/";
+            }
+            {
+              name = "wiki";
+              tags = ["wiki" "nix"];
+              url = "https://wiki.nixos.org/";
+            }
+          ];
+        }
+      ];
+
+      # TODO: containers = {};
+
+      extensions = with pkgs.nur.repos.rycee.firefox-addons; [
+      ];
+    };
   };
 }
