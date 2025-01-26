e1dc314dfe77456e03814be3ddce608b2466aa91
Sk7Str1pe <finenkofedor@gmail.com>
2025-01-26 14:25:17 +0300

    upd

--- home.nix
+++ home.nix
@@ -33,7 +33,7 @@
   # You should not change this value, even if you update Home Manager. If you do
   # want to update the value, then make sure to first check the Home Manager
   # release notes.
-  home.stateVersion = "24.05"; # Please read the comment before changing.
+  home.stateVersion = "25.05"; # Please read the comment before changing.
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
@@ -57,7 +57,7 @@
     inputs.nix-software-center.packages.${system}.nix-software-center
     inputs.nixos-conf-editor.packages.${system}.nixos-conf-editor
     ayugram-desktop
-    wineWowPackages.stagingFull
+    wineWow64Packages.stagingFull
     winetricks
 
     # # You can also create simple shell scripts directly inside your
