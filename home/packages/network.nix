c4c080ad019b61c11735589297f2784a6f86ec3b
Sk7Str1pe <finenkofedor@gmail.com>
2025-01-29 21:11:25 +0300

    network.nix => remove hiddify and nekoray

--- home/packages/network.nix
+++ home/packages/network.nix
@@ -1,13 +1,11 @@
 {pkgs, ...}: {
   config.home.packages = with pkgs; [
-    hiddify-app
-    nekoray
-    vesktop
+    #vesktop // installed with nixcord module
     arrpc
     transmission_3-gtk
     qbittorrent-enhanced
     samba
     ayugram-desktop
-    #firefox # installed with home-manager module
+    #firefox // installed with home-manager module
   ];
 }
