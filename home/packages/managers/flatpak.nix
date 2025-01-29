66c64a69641bc32d9b452ef7808aafa13b12e844
Sk7Str1pe <finenkofedor@gmail.com>
2025-01-29 21:13:18 +0300

    flatpak.nix => configure flatpak

---
+++ home/packages/managers/flatpak.nix
@@ -0,0 +1,15 @@
+{
+  services.flatpak = {
+    enable = false;
+    remotes = [
+      {
+        name = "flathub";
+        location = "https://flathub.org/repo/flathub.flatpakrepo";
+      }
+      {
+        name = "flathub-beta";
+        location = "https://flathub.org/beta-repo/flathub-beta.flatpakrepo";
+      }
+    ];
+  };
+}
