1be2e7d6649cd5b65d2702a8a603941af29de8e9
Sk7Str1pe <finenkofedor@gmail.com>
2025-01-30 17:03:03 +0300

    discord.nix => better configuration

--- web/discord.nix
+++ web/discord.nix
@@ -1,6 +1,5 @@
 {pkgs, ...}: {
   services.arrpc.enable = true;
-
   programs.nixcord = {
     enable = true;
     discord.enable = false;
@@ -9,16 +8,35 @@
       package = pkgs.vesktop.override {
         withTTS = false;
         withMiddleClickScroll = true;
-        withSystemVencord = true;
+        #withSystemVencord = true;
+      };
+      settings = {
+        "discordBranch" = "ptb";
+        "minimizeToTray" = true;
+        "arRPC" = true;
+        "splashColor" = "rgb(205, 214, 244)";
+        "splashBackground" = "rgb(30, 30, 46)";
+        "splashTheming" = true;
+        "disableMinSize" = true;
+        "openLinksWithElectron" = true;
       };
     };
     quickCss = "@import url(\"https://catppuccin.github.io/discord/dist/catppuccin-mocha-mauve.theme.css\")";
     vesktopConfig = {
       useQuickCss = true;
-
       themeLinks = [
         "https://catppuccin.github.io/discord/dist/catppuccin-mocha-mauve.theme.css"
       ];
+      plugins = {
+        accountPanelServerProfile.enable = true;
+        alwaysAnimate.enable = true;
+        voiceMessages = {
+          enable = true;
+          noiseSuppression = true;
+          echoCancellation = true;
+        };
+        webRichPresence.enable = true;
+      };
     };
   };
 }
