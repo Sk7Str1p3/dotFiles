674af97ce65807dd1cf1ab52dfee33e9fc31016d
Sk7Str1pe <finenkofedor@gmail.com>
2025-02-02 17:16:55 +0300

    games.nix => install GTK steam and rpc bridge

--- home/packages/games.nix
+++ home/packages/games.nix
@@ -1,10 +1,18 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  inputs,
+  ...
+}: let
+  gamePkgs = inputs.nix-gaming.packages.${pkgs.hostPlatform.system};
+in {
   config = {
     home.packages = with pkgs; [
-      steam
+      adwsteamgtk
       bottles
       wineWow64Packages.stableFull
       winetricks
+
+      gamePkgs.wine-discord-ipc-bridge
     ];
   };
 }
