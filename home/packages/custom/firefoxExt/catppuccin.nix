c8f7b4eed5e455c91d3361adedf63c233861e08f
Sk7Str1pe <finenkofedor@gmail.com>
2025-02-03 19:53:25 +0300

    fix catppuccin firefox plugin

--- home/packages/custom/firefoxExt/catppuccin.nix
+++ home/packages/custom/firefoxExt/catppuccin.nix
@@ -1,11 +1,17 @@
 {
   buildFirefoxXpiAddon,
-  #lib
+  lib,
 }:
 buildFirefoxXpiAddon rec {
   pname = "catppuccin-firefox";
   version = "2.0";
   addonId = "{76aabc99-c1a8-4c1e-832b-d4f2941d5a7a}";
   url = "https://addons.mozilla.org/firefox/downloads/file/3990325/catppuccin_mocha_mauve_git-${version}.xpi";
-  sha256 = "sha256-age3Nj2n4X3Zg1bTElChOpgDDGCQd4nOqHebW8zNsBQ=";
+  sha256 = "sha256-pSlzVe7XbTbrC76iAinYrr7qIl69OpH3Wk00MoAIe74=";
+  meta = with lib; {
+    homepage = "https://github.com/catppuccin/firefox";
+    description = "Soothing pastel theme for firefox!";
+    license = licenses.mit;
+    platforms = platforms.all;
+  };
 }
