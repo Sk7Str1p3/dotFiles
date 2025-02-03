47ee25f16b2958f66e79674d0a6f397844f5e3b6
Sk7Str1pe <finenkofedor@gmail.com>
2025-02-03 23:45:08 +0300

    pkgs: add sfp to list of packages

--- home/packages/custom/default.nix
+++ home/packages/custom/default.nix
@@ -9,4 +9,25 @@
       inherit (inputs.firefox-addons.lib."x86_64-linux") buildFirefoxXpiAddon;
     };
   };
+  SFP-bin = pkgs.callPackage ./steam/SFP/binary.nix {
+    inherit
+      (pkgs)
+      stdenv
+      vistafonts
+      zlib
+      icu
+      openssl
+      ;
+  };
+  SFP = pkgs.callPackage ./steam/SFP/package.nix {
+    inherit
+      (pkgs)
+      buildDotnetModule
+      dotnetCorePackages
+      stdenv
+      zlib
+      icu
+      openssl
+      ;
+  };
 }
