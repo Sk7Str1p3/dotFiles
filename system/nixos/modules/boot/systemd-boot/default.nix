a032c13c7feec7424416f2653692553ff39efd8c
Sk7Str1pe <finenkofedor@gmail.com>
2025-02-22 20:22:30 +0300

    feat: adapt bootloader configuration into modules

---
+++ system/nixos/modules/boot/systemd-boot/default.nix
@@ -0,0 +1,36 @@
+{
+  config,
+  lib,
+  ...
+}:
+with lib; let
+  cfg = config.module.boot.systemd-boot;
+in {
+  # Declare module options
+  options = {
+    module.boot.systemd-boot = {
+      enable = mkEnableOption "systemd-boot";
+    };
+  };
+
+  # Implementation
+  config = mkIf cfg.enable {
+    boot = {
+      loader = {
+        efi = {
+          # EFI interaction
+          canTouchEfiVariables = true;
+          efiSysMountPoint = "/boot";
+        };
+        # Systemd-boot configuration
+        systemd-boot = {
+          enable = true;
+          configurationLimit = 20;
+          consoleMode = "max";
+          editor = false;
+        };
+        timeout = 2;
+      };
+    };
+  };
+}
