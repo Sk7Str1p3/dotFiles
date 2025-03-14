07ae75438775b08fca2a7134883cfbd02cf178cb
Sk7Str1pe <finenkofedor@gmail.com>
2025-03-14 17:23:14 +0000

    chore: disable nh cleaner to prevent config

--- user/home-modules/shell/nh/default.nix
+++ user/home-modules/shell/nh/default.nix
@@ -17,11 +17,6 @@ in {
   config = mkIf cfg.enable {
     programs.nh = {
       enable = true;
-      clean = {
-        enable = true;
-        dates = "weekly";
-        extraArgs = "--keep 5 --keep-since 3d";
-      };
       flake = "${config.home.homeDirectory}/Documents/dotFiles";
     };
   };
