cec78bc5bfa44ff07c9f0e6b2b50b4b597d35e33
Sk7Str1pe <finenkofedor@gmail.com>
2025-03-15 17:20:06 +0000

    home: yazi: ouch

--- user/home-modules/shell/yazi/default.nix
+++ user/home-modules/shell/yazi/default.nix
@@ -66,6 +66,52 @@ in {
           ];
         };
       };
+      ouch = {
+        plugins.ouch = pkgs.callPackage ./plugins/ouch/package.nix {};
+        settings = {
+          plugin.prepend_previewers = [
+            {
+              mime = "application/*zip";
+              run = "ouch";
+            }
+            {
+              mime = "application/x-tar";
+              run = "ouch";
+            }
+            {
+              mime = "application/x-bzip2";
+              run = "ouch";
+            }
+            {
+              mime = "application/x-7z-compressed";
+              run = "ouch";
+            }
+            {
+              mime = "application/x-rar";
+              run = "ouch";
+            }
+            {
+              mime = "application/x-xz";
+              run = "ouch";
+            }
+          ];
+          opener.extract = [
+            {
+              run = "ouch d -y \"$@\"";
+              desc = "Extract here with ouch";
+            }
+          ];
+        };
+        keymap = {
+          manager.pretend_keymap = [
+            {
+              run = "plugin ouch --args=zip";
+              on = ["C"];
+              desc = "Compress with ouch";
+            }
+          ];
+        };
+      };
     });
     #TODO: more detailed configuration
   };
