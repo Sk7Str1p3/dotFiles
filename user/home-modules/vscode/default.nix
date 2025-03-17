da23196faee8153e2acc2fa0f30cb9d050584fe6
Sk7Str1p3 <finenkofedor@gmail.com>
2025-03-17 17:17:08 +0000

    home: vsc: fix eval warning

--- user/home-modules/vscode/default.nix
+++ user/home-modules/vscode/default.nix
@@ -19,71 +19,73 @@ in {
     programs.vscode = {
       enable = true;
       package = pkgs.stable.vscode;
-      # Disable some annoying and not ''nix way'' stuff
-      enableExtensionUpdateCheck = false;
-      enableUpdateCheck = false;
       mutableExtensionsDir = false;
-      extensions = with pkgs.stable.vscode-extensions;
-        [
-          # VSC generic extensions
-          catppuccin.catppuccin-vsc
-          leonardssh.vscord
-          github.copilot
-          github.copilot-chat
+      # Disable some annoying and not ''nix way'' stuff
+      profiles.default = {
+        enableExtensionUpdateCheck = false;
+        enableUpdateCheck = false;
+        extensions = with pkgs.stable.vscode-extensions;
+          [
+            # VSC generic extensions
+            catppuccin.catppuccin-vsc
+            leonardssh.vscord
+            github.copilot
+            github.copilot-chat
 
-          # Better Git integration
-          mhutchie.git-graph
-          github.vscode-pull-request-github
-          eamodio.gitlens
+            # Better Git integration
+            mhutchie.git-graph
+            github.vscode-pull-request-github
+            eamodio.gitlens
 
-          # Nix-specific extensions for better workflow
-          bbenoist.nix
-          kamadorueda.alejandra
-          jnoortheen.nix-ide
-          jeff-hykin.better-nix-syntax
-          arrterian.nix-env-selector
-          mkhl.direnv
+            # Nix-specific extensions for better workflow
+            bbenoist.nix
+            kamadorueda.alejandra
+            jnoortheen.nix-ide
+            jeff-hykin.better-nix-syntax
+            arrterian.nix-env-selector
+            mkhl.direnv
 
-          # C workflow
-          ms-vscode.cmake-tools
+            # C workflow
+            ms-vscode.cmake-tools
 
-          # Rust workflow # TODO: wait for fix on rust-analyzer; for now replaced with custom extension
-          # rust-lang.rust-analyzer
-          fill-labs.dependi
-          vadimcn.vscode-lldb
+            # Rust workflow # TODO: wait for fix on rust-analyzer; for now replaced with custom extension
+            # rust-lang.rust-analyzer
+            fill-labs.dependi
+            vadimcn.vscode-lldb
 
-          # Python workflow
-          ms-python.python
-          ms-python.flake8
-          ms-python.black-formatter
-          matangover.mypy
+            # Python workflow
+            ms-python.python
+            ms-python.flake8
+            ms-python.black-formatter
+            matangover.mypy
 
-          dbaeumer.vscode-eslint
-          chrischinchilla.vscode-pandoc
-        ]
-        ++ (with pkgs.vscode-marketplace-release; [
-          rust-lang.rust-analyzer
-        ]);
-      # TODO: Add more extensions
-      userSettings = {
-        "workbench.colorTheme" = "Catppuccin Mocha";
+            dbaeumer.vscode-eslint
+            chrischinchilla.vscode-pandoc
+          ]
+          ++ (with pkgs.vscode-marketplace-release; [
+            rust-lang.rust-analyzer
+          ]);
+        # TODO: Add more extensions
+        userSettings = {
+          "workbench.colorTheme" = "Catppuccin Mocha";
 
-        "editor.fontFamily" = "'JetBrainsMono Nerd Font Propo'";
-        "editor.fontLigatures" = true;
-        "editor.fontSize" = 16;
-        "editor.minimap.sectionHeaderFontSize" = 16;
-        "editor.suggestFontSize" = 16;
+          "editor.fontFamily" = "'JetBrainsMono Nerd Font Propo'";
+          "editor.fontLigatures" = true;
+          "editor.fontSize" = 16;
+          "editor.minimap.sectionHeaderFontSize" = 16;
+          "editor.suggestFontSize" = 16;
 
-        "terminal.integrated.fontSize" = 16;
-        "terminal.integrated.fontFamily" = "'JetBrainsMono Nerd Font Propo'";
-        "terminal.integrated.fontLigatures" = true;
+          "terminal.integrated.fontSize" = 16;
+          "terminal.integrated.fontFamily" = "'JetBrainsMono Nerd Font Propo'";
+          "terminal.integrated.fontLigatures" = true;
 
-        "git.autofetch" = true;
-        "git.enableSmartCommit" = true;
-        "git.confirmSync" = false;
+          "git.autofetch" = true;
+          "git.enableSmartCommit" = true;
+          "git.confirmSync" = false;
 
-        "nix.enableLanguageServer" = true;
-        "nix.serverPath" = "nixd";
+          "nix.enableLanguageServer" = true;
+          "nix.serverPath" = "nixd";
+        };
       };
     };
     home.file.".vscode/argv.json".text = builtins.toJSON {
