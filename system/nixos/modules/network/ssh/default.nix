7f58876b94d6d452d5131a804b7201577a290ad3
Sk7Str1p3 <finenkofedor@gmail.com>
2025-05-01 20:17:27 +0000

    nixos: ssh module

---
+++ system/nixos/modules/network/ssh/default.nix
@@ -0,0 +1,66 @@
+{
+  lib,
+  self,
+  config,
+  hostName,
+  ...
+}:
+with lib;
+let
+  cfg = config.module.system.network.ssh;
+in
+{
+  options = {
+    module.system.network.ssh = {
+      enable = mkEnableOption ''
+        SSH - Secure Shell
+      '';
+      keyAlgorithm = mkOption {
+        type = types.either (types.listOf (
+          types.enum [
+            "ecdsa"
+            "ed25519"
+            "rsa"
+          ]
+        )) (types.enum [ null ]);
+        default = [ "ed25519" ];
+        description = ''
+          Which types of keys should be unpacked from secrets
+          If not "none" keys must be present at secrets/hosts/{HOSTNAME}/sshKeys
+        '';
+      };
+    };
+  };
+
+  config = mkIf cfg.enable (mkMerge [
+    {
+      services.openssh = {
+        enable = true;
+        settings = {
+          PermitRootLogin = "no";
+          PasswordAuthentication = false;
+          KbdInteractiveAuthentication = false;
+        };
+        openFirewall = true;
+      };
+    }
+
+    (mkIf (cfg.keyAlgorithm != null) {
+      sops.secrets = foldl' (acc: key: acc // key) { } (
+        map (algo: {
+          "ssh/${algo}/pub" = {
+            sopsFile = "${self}/secrets/hosts/${hostName}/sshKeys/${algo}.yaml";
+            mode = "0444";
+          };
+          "ssh/${algo}/key".sopsFile = "${self}/secrets/hosts/${hostName}/sshKeys/${algo}.yaml";
+        }) cfg.keyAlgorithm
+      );
+      environment.etc = foldl' (acc: key: acc // key) { } (
+        map (algo: {
+            "ssh/ssh_host_${algo}_key".source = config.sops.secrets."ssh/${algo}/key".path;
+            "ssh/ssh_host_${algo}_key.pub".source = config.sops.secrets."ssh/${algo}/pub".path;
+        }) cfg.keyAlgorithm
+      );
+    })
+  ]);
+}
