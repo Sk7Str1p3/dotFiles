64429ce119c965ef06520a756afd97fecd02bb9b
Sk7Str1p3 <finenkofedor@gmail.com>
2025-05-09 20:03:38 +0000

    home: impermanence module

---
+++ user/home-modules/home/impermanence/default.nix
@@ -0,0 +1,70 @@
+{
+  lib,
+  osConfig,
+  config,
+  ...
+}:
+with lib;
+let
+  cfg = config.module.user.impermanence;
+in
+{
+  options = {
+    module.user.impermanence = {
+      enable = mkOption {
+        type = types.bool;
+        default = osConfig.module.system.impermanence.enable;
+        description = ''
+          Whether to enable Impermanence
+        '';
+      };
+      extraFiles = mkOption {
+        type = types.listOf types.anything;
+        default = [ ];
+        description = ''
+          Extra files which should not be removed on reboot
+        '';
+      };
+      extraDirs = mkOption {
+        type = types.listOf types.anything;
+        default = [ ];
+        description = ''
+          Extra directories which should not be removed on reboot
+        '';
+      };
+    };
+  };
+
+  config = mkIf cfg.enable {
+    home.persistence = {
+      "/nix/persist/user/${config.home.username}" = {
+        enable = true;
+        allowOther = true;
+        defaultDirectoryMethod = "bindfs";
+        directories = [
+        "Desktop"
+        "Documents"
+        "Downloads"
+        "Music"
+        "Pictures"
+        "Videos"
+        ".local/share/Steam"
+        ".local/share/PrismLauncher"
+        ".local/share/AyuGramDesktop"
+
+        ".cargo"
+
+        ".mozilla" # TODO: set up firefox with nix
+        ".local/share/Trash"
+        ".local/share/bottles"
+        ".local/share/keyrings"
+        ".local/share/direnv"
+        ] ++ cfg.extraDirs;
+        files = [
+        ".local/share/fish/fish_history"
+        ".config/onefetch/config.toml"
+        ] ++ cfg.extraFiles;
+      };
+    };
+  };
+}
